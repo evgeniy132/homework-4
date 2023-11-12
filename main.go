635b1cb81fc02cc4e3a439cdefb7f7bbbb1d2477
(http://user.name/) <(http://user.email/)>
2023-11-12 17:38:08 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func averageMarks(scores []float64) float64 {
+	sum := 0.0
+	for _, score := range scores {
+		sum += score
+	}
+	average := sum / float64(len(scores))
+	return average
+}
+
+func main() {
+	scores := []float64{78.5, 82.0, 90.5, 85.5, 89.0}
+	average := averageMarks(scores)
+	fmt.Printf("Average: %.2f\n", average)
+}
